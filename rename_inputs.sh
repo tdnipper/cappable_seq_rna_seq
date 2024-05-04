116d4e441054b67d968adeeda1500ad1ad82d2d3
Thomas Nipper <nipper@wisc.edu>
2024-05-04 15:19:18 +0000

    refactor: Improve file renaming script for sequencing data

--- rename_inputs.sh
+++ rename_inputs.sh
@@ -2,10 +2,11 @@
 
 # Rename files in raw_data directory to remove the sequencing core processing info from the file name
 
-
-for file in $(find raw_data -name ".fastq.gz"); do
-    basename=$(basename $file)
-    newname=${basename%%_*}
-    new_path=$(dirname $file)/${newname}.fastq.gz
-    mv $file $new_path
+for file in $(find raw_data -name "*.fastq.gz"); do 
+    basename=$(basename $file ".fastq.gz")
+    newname=${basename%%_*} # remove everything after first '_'
+    replicate=${basename%_*} # remove everything after basename last '_'
+    replicate=${replicate##*_} # remove everything before replicate last '_'
+    new_path=$(dirname $file)/${newname}_${replicate}.fastq.gz # construct new path
+    mv $file $new_path # rename file
 done
