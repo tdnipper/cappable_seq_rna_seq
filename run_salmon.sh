3ac2a9d09ea182377d6b3f19601a4caae506b72a
Thomas Nipper <nipper@wisc.edu>
2024-05-07 16:19:15 +0000

    Run salmon on ribodepleted reads in mapping mode

--- run_salmon.sh
+++ run_salmon.sh
@@ -7,16 +7,23 @@ if [ ! -d salmon_quantifications ]; then
     sudo chown $USER_ID:$USER_ID salmon_quantifications
 fi
 
-if [! -f salmon_index ]; then
-    salmon index -t genome/transcriptome.fasta \
-        -i salmon_quantifications/salmon_index
+if [ ! -d genome/salmon_index ]; then
+salmon index \
+    -t genome/hybrid_gentrome.fasta \
+    -d genome/decoys.txt \
+    -p 4 \
+    -i genome/salmon_index
 fi
 
-for file in $(find ribodepleted_star_alignments/ -name "*_transcriptome_sorted.bam"); do
-    name = $(basename $file "_ribodepleted_transcriptome_sorted.bam")
+for file in $(find ribodepleted_reads/ -name "*trimmed_nonrRNA.fq.gz"); do
+    name=$(basename $file "_trimmed_nonrRNA.fq.gz")
     echo "Quantifying ${name}"
     salmon quant \
-        -i salmon_quantifications/salmon_index \
+        -i genome/salmon_index \
         -l A \
-        -1 $file \
+        -r $file \
+        --validateMappings \
+        -p 4 \
+        -o salmon_quantifications/${name}
+done
 
