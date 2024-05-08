435dd4617ce29adb589d7e9af69f8873ee11bee9
Thomas Nipper <nipper@wisc.edu>
2024-05-08 00:00:46 +0000

    Depreciated in favor of run_sortmeRNA.py

--- sortRNA.sh
+++ sortRNA.sh
@@ -2,11 +2,12 @@
 
 source env.sh
 
-sudo mkdir ribodepleted_reads
+sudo mkdir ribodepleted_reads/
 sudo chown $USER_ID:$GROUP_ID ribodepleted_reads
 sudo chmod -R 775 ribodepleted_reads
 
-for file in $(find trimmed_reads/ -name "*.fastq.gz"); do
+# Sort rRNA reads
+for file in $(find trimmed_reads/ -name "*_R1_trimmed.fastq.gz"); do
     base=$(basename $file ".fastq.gz")
     echo "Sorting $base"
     sortmerna -ref human_rRNAs.fasta -reads $file --threads 4 -fastx -workdir ribodepleted_reads/$base -aligned ribodepleted_reads/$base"_rRNA" -other ribodepleted_reads/$base"_nonrRNA"
