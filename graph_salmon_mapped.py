9d635bc7a62f4cec1721d44ee637ea4f00e8ef22
Thomas Nipper <nipper@wisc.edu>
2024-05-13 21:24:25 +0000

    add star data

--- graph_salmon_mapped.py
+++ graph_salmon_mapped.py
@@ -10,6 +10,9 @@ with_cleanup = pd.read_csv("multiqc_test/Cappable-seq-pilot_multiqc_report_data/
 trinity = pd.read_csv("multiqc_test/Cappable-seq-pilot_multiqc_report_data/multiqc_salmon_salmon_mapped_to_trinity.txt",
                          sep="\t")
 
+star = pd.read_csv("multiqc_test/Cappable-seq-pilot_multiqc_report_data/multiqc_star_star.txt",
+                            sep="\t")
+
 def graph(data, title):
     sns.set_theme(style="whitegrid")
     plt.figure(figsize=(10, 6))
@@ -27,3 +30,15 @@ def graph(data, title):
 graph(no_cleanup, "No Cleanup")
 graph(with_cleanup, "With Cleanup")
 graph(trinity, "Trinity")
+
+# Include STAR as one off
+sns.set_theme(style="whitegrid")
+plt.figure(figsize=(10, 6))
+ax = sns.barplot(x="Sample", y="uniquely_mapped_percent", data=star)
+ax.set_title("STAR Reads Aligned to genome")
+ax.set_ylabel("Percent of Reads Aligned")
+ax.set_xlabel("Sample")
+plt.xticks(rotation=45)
+plt.ylim(0, 100)
+plt.tight_layout()
+plt.savefig("STAR_mapped.png", dpi=300)
