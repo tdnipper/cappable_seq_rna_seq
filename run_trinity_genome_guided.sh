cf489945483cc48a15132f20d3233a949b8133e9
Thomas Nipper <nipper@wisc.edu>
2024-05-20 21:38:22 -0500

    update filepaths to follow trinity_genome_guided rename

--- run_trinity_genome_guided.sh
+++ run_trinity_genome_guided.sh
@@ -4,8 +4,8 @@ CPU=8
 
 # genome guided trinity on merged bam file
 
-Trinity --genome_guided_bam trinity/merged_sorted.bam \
+Trinity --genome_guided_bam trinity_genome_guided/merged_sorted.bam \
     --genome_guided_max_intron 1000000 \
     --max_memory 30G \
     --CPU $CPU \
-    --output trinity/
+    --output trinity_genome_guided/
