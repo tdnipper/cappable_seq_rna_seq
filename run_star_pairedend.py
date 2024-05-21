c71c4b2a53c68d1bb27803b371d47166e31692da
Thomas Nipper <nipper@wisc.edu>
2024-05-21 09:20:33 -0500

    Convert all run commands to fstrings and add autodetect cpu

--- run_star_pairedend.py
+++ run_star_pairedend.py
@@ -13,6 +13,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 PUID = os.getuid()
 PGID = os.getgid()
 reads_dir = f"{basedir}/bbduk_reads"
+CPU = os.cpu_count()
 
 if not os.path.exists(reads_dir):
     raise FileNotFoundError(
@@ -31,7 +32,9 @@ if not os.path.exists(star_index_dir):
     )
 
 # reads = get_filenames(reads_dir)
-reads = get_filenames_filepaths(reads_dir, "_R1", "_R2", file_filter=lambda x: "nonrRNA" in x)
+reads = get_filenames_filepaths(
+    reads_dir, "_R1", "_R2", file_filter=lambda x: "nonrRNA" in x
+)
 
 
 for key in reads:
@@ -40,110 +43,69 @@ for key in reads:
 
 # Run star alignment on each sample in paired-end mode
 for key in reads:
-        keyname = key # used to split key, not needed now but keeping syntax the same
-        result = subprocess.run(
-            [
-                "STAR",
-                "--runThreadN",
-                "4",
-                "--genomeDir",
-                star_index_dir,
-                "--readFilesIn",
-                f"{reads[key][0]}",
-                f"{reads[key][1]}",
-                "--outFileNamePrefix",
-                f"{star_dir}/{keyname}/{keyname}_",
-                "--quantMode",
-                "TranscriptomeSAM",
-                "--genomeLoad",
-                "LoadAndKeep",
-                "--outReadsUnmapped",
-                "Fastx",
-                "--readFilesCommand",
-                "zcat",
-            ]
+    keyname = key  # used to split key, not needed now but keeping syntax the same
+    result = subprocess.run(
+        f"STAR \
+                --runThreadN {CPU} \
+                --genomeDir {star_index_dir} \
+                --readFilesIn {reads[key][0]} {reads[key][1]} \
+                --outFileNamePrefix {star_dir}/{keyname}/{keyname}_ \
+                --quantMode TranscriptomeSAM \
+                --genomeLoad LoadAndKeep \
+                --outReadsUnmapped Fastx \
+                --readFilesCommand zcat",
+        shell=True,
+        check=True,
+    )
+    if result.returncode != 0:
+        raise RuntimeError(f"Error: STAR failed to align {key}")
+    # Sort and index bam files
+    result = subprocess.run(
+        f"samtools view {star_dir}/{keyname}/{keyname}_Aligned.out.sam -o {star_dir}/{keyname}/{keyname}_aligned.bam",
+        shell=True,
+        check=True,
+    )
+    if result.returncode != 0:
+        raise RuntimeError(f"Error: samtools failed to convert {key} to bam")
+    os.remove(f"{star_dir}/{keyname}/{keyname}_Aligned.out.sam")
+    result = subprocess.run(
+        f"samtools sort {star_dir}/{keyname}/{keyname}_Aligned.toTranscriptome.out.bam -o {star_dir}/{keyname}/{keyname}_Aligned.toTranscriptome.sorted.bam -@ {CPU}",
+        shell=True,
+        check=True,
+    )
+    if result.returncode != 0:
+        raise RuntimeError(
+            f"Error: samtools failed to sort {key}_Aligned.toTranscriptome.out.bam"
         )
-        if result.returncode != 0:
-            raise RuntimeError(f"Error: STAR failed to align {key}")
-        # Sort and index bam files
-        result = subprocess.run(
-            [
-                "samtools",
-                "view",
-                f"{star_dir}/{keyname}/{keyname}_Aligned.out.sam",
-                "-o",
-                f"{star_dir}/{keyname}/{keyname}_aligned.bam",
-            ]
+    result = subprocess.run(
+        f"samtools sort {star_dir}/{keyname}/{keyname}_aligned.bam -o {star_dir}/{keyname}/{keyname}_coord_sorted.bam -@ {CPU}",
+        shell=True,
+        check=True,
+    )
+    if result.returncode != 0:
+        raise RuntimeError(f"Error: samtools failed to sort {key}_aligned.bam")
+    result = subprocess.run(
+        f"samtools index {star_dir}/{keyname}/{keyname}_Aligned.toTranscriptome.sorted.bam -@ {CPU}",
+        shell=True,
+        check=True,
+    )
+    if result.returncode != 0:
+        raise RuntimeError(
+            f"Error: samtools failed to index {key}_Aligned.toTranscriptome.sorted.bam"
         )
-        if result.returncode != 0:
-            raise RuntimeError(f"Error: samtools failed to convert {key} to bam")
-        os.remove(f"{star_dir}/{keyname}/{keyname}_Aligned.out.sam")
-        result = subprocess.run(
-            [
-                "samtools",
-                "sort",
-                f"{star_dir}/{keyname}/{keyname}_Aligned.toTranscriptome.out.bam",
-                "-o",
-                f"{star_dir}/{keyname}/{keyname}_Aligned.toTranscriptome.sorted.bam",
-                "-@",
-                "4",
-            ]
-        )
-        if result.returncode != 0:
-            raise RuntimeError(
-                f"Error: samtools failed to sort {key}_Aligned.toTranscriptome.out.bam"
-            )
-        result = subprocess.run(
-            [
-                "samtools",
-                "sort",
-                f"{star_dir}/{keyname}/{keyname}_aligned.bam",
-                "-o",
-                f"{star_dir}/{keyname}/{keyname}_coord_sorted.bam",
-                "-@",  # for dirpath, dirnames, filenames in os.walk(reads_dir):
-                "4",
-            ]
-        )
-        if result.returncode != 0:
-            raise RuntimeError(f"Error: samtools failed to sort {key}_aligned.bam")
-        result = subprocess.run(
-            [
-                "samtools",
-                "index",
-                f"{star_dir}/{keyname}/{keyname}_Aligned.toTranscriptome.sorted.bam",
-                "-@",
-                "4",
-            ]
-        )
-        if result.returncode != 0:
-            raise RuntimeError(
-                f"Error: samtools failed to index {key}_Aligned.toTranscriptome.sorted.bam"
-            )
-        result = subprocess.run(
-            [
-                "samtools",
-                "index",
-                f"{star_dir}/{keyname}/{keyname}_coord_sorted.bam",
-                "-@",
-                "4",
-            ]
-        )
-        if result.returncode != 0:
-            raise RuntimeError(
-                f"Error: samtools failed to index {key}_coord_sorted.bam"
-            )
+    result = subprocess.run(
+        f"samtools index {star_dir}/{keyname}/{keyname}_coord_sorted.bam -@ {CPU}",
+        shell=True,
+        check=True,
+    )
+    if result.returncode != 0:
+        raise RuntimeError(f"Error: samtools failed to index {key}_coord_sorted.bam")
 
 # Remove genome from memory
 result = subprocess.run(
-    [
-        "STAR",
-        "--genomeLoad",
-        "Remove",
-        "--genomeDir",
-        star_index_dir,
-        "--outFileNamePrefix",
-        f"{star_dir}/exit/exit",
-    ]
+    f"STAR --genomeLoad Remove --genomeDir {star_index_dir} --outFileNamePrefix {star_dir}/exit/exit",
+    shell=True,
+    check=True,
 )
 if result.returncode != 0:
     raise RuntimeError(f"Error: STAR failed to remove genome from memory")
