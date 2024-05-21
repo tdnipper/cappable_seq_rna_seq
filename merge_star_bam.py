77c301b17c4d653104fcad9bc677e47cfb209762
Thomas Nipper <nipper@wisc.edu>
2024-05-20 21:13:26 -0500

    Fix bam files list join for samtools merge

--- merge_star_bam.py
+++ merge_star_bam.py
@@ -3,10 +3,11 @@ import subprocess
 
 PUID = os.getuid()
 PGID = os.getgid()
+trinity_dir = "trinity_genome_guided"
 
-if not os.path.exists("trinity"):
-    os.makedirs("trinity")
-    os.chown("trinity", PUID, PGID)
+if not os.path.exists(trinity_dir):
+    os.makedirs(trinity_dir)
+    os.chown(trinity_dir, PUID, PGID)
 
 if not os.path.exists("star_alignment"):
     raise FileNotFoundError("Star alignment directory not at './star_alignment'")
@@ -18,19 +19,21 @@ for dirpath, dirnames, filenames in os.walk("star_alignment"):
         if "_aligned.bam" in filename:
             bam_files.append(os.path.join(dirpath, filename))
 
+bam_files_str = " ".join(bam_files)
+
 # Merge bam files
-subprocess.run(["samtools",
-                "merge",
-                "-o",
-                "trinity/merged.bam",
-                *bam_files
-                ])
+result = subprocess.run(f"samtools \
+                merge \
+                -o {trinity_dir}/merged.bam \
+                *{bam_files_str} \
+                -@ 4",
+                shell=True)
+if result.returncode != 0:
+    raise ValueError("Error merging bam files")
 
 # Sort bam file by coordinate
-subprocess.run(["samtools",
-                "sort",
-                "-o",
-                "trinity/merged_sorted.bam",
-                "trinity/merged.bam",
-                "-@",
-                "4"])
+subprocess.run(f"samtools \
+                sort \
+                -o {trinity_dir}/merged_sorted.bam \
+                {trinity_dir}/merged.bam \
+                -@ 4")
