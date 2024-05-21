9899eb0ecfcec22cc2f37592bd966b0efaf801ab
Thomas Nipper <nipper@wisc.edu>
2024-05-21 17:11:40 -0500

    MAJOR UPDATE: create classes FileHandler and ShellProcessRunner to handle repetitive functions and call them in corresponding scripts instead of calling functions like get_filenames_filepaths() from module. Class functions have built in error handling

--- filename_utils.py
+++ filename_utils.py
@@ -4,6 +4,7 @@ __author__ = "Thomas"
 __date__ = "2024-05-08" 
 
 import os
+import subprocess
 
 # Assumes prefixes are the same for read files with _R1 and _R2 denoting fwd and rev reads
 
@@ -60,3 +61,71 @@ def get_filenames_filepaths(directory: str, prefix1: str = "_R1", prefix2: str =
                     files[name][1] = os.path.join(dirpath, file)
                 
     return files
+
+class FileHandler:
+    def __init__(self, path):
+        self.path = path
+    
+    def check_exists(self, error_message: str = None):
+        try:
+            os.path.exists(self.path)
+        except FileNotFoundError as e:
+            if error_message is not None:
+                raise FileNotFoundError(error_message)
+            else:
+                raise FileNotFoundError(f"Error: {e}")
+        return os.path.exists(self.path)
+    
+    def make_dir(self):
+        if not self.check_exists():
+            os.makedirs(self.path)
+            os.chown(self.path, os.getuid(), os.getgid())
+            return self
+    
+    def get_files(self, prefix1: str = "_R1", prefix2: str = "_R2", file_filter: callable = None):
+        """
+        Retrieves the filenames and filepaths from a given directory.
+
+        Args:
+            directory (str): The directory to search for files.
+            prefix1 (str, optional): The prefix for the first set of files. Defaults to "_R1", but can be changed to 'fwd' for example.
+            prefix2 (str, optional): The prefix for the second set of files. Defaults to "_R2", but can be changed to 'rev' for example.
+            file_filter (function, optional): A function to filter files. Defaults to None. Can be used to filter out log files or non fastq, etc.
+
+        Returns:
+            dict: A dictionary containing the sample names and their corresponding filepaths.
+                The keys of the dictionary are the filenames split at the prefixes, and the values are nested dictionaries.
+                The nested dictionaries have keys 0 and 1, representing the first and second set of files respectively.
+                The values of the nested dictionaries are the filepaths.
+        
+        Example:
+            To find filenames with prefixes R1 and R2 in the directory 'trimmed_reads', excluding log files:
+            get_filenames_filepaths('trimmed_reads', '_R1', '_R2', file_filter = lambda x: '.log' not in x)
+
+        """
+        files = {}
+        for dirpath, _, filenames in os.walk(self.path):
+            for file in filenames:
+                if file_filter is None or file_filter(file):
+                    if prefix1 in file:
+                        name = file.split(prefix1)[0]
+                    elif prefix2 in file:
+                        name = file.split(prefix2)[0]
+                    if name not in files:
+                        files[name] = {}
+                    if prefix1 in file:
+                        files[name][0] = os.path.join(dirpath, file)
+                    elif prefix2 in file:
+                        files[name][1] = os.path.join(dirpath, file)
+                
+        return files
+
+class ShellProcessRunner:
+    def __init__(self, command):
+        self.command = command
+
+    def run_shell(self):
+        try:
+            subprocess.run(self.command, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            raise Exception(f"Error: {e}")
