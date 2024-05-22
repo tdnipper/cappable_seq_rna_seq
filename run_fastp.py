3710645cb096362587c0d7a34edd708e520e19d1
Thomas Nipper <nipper@wisc.edu>
2024-05-22 16:58:03 -0500

    Depreciate cutadapt in favor of fastp because of auto adapter generation

---
+++ run_fastp.py
@@ -0,0 +1,26 @@
+from filename_utils import FileHandler
+from filename_utils import ShellProcessRunner
+import os
+
+input_dir = "raw_data"
+output_dir = "trimmed_reads_fastp"
+CPU = os.cpu_count()
+
+output_dir_handler = FileHandler(output_dir)
+output_dir_handler.make_dir()
+
+input_handler = FileHandler(input_dir)
+files = FileHandler.get_files(input_handler, "_R1", "_R2")
+
+for key in files:
+    fastp_runner = ShellProcessRunner(f"fastp -i {files[key][0]} \
+        -I {files[key][1]} \
+        -o {output_dir}/{key}_R1_trimmed.fastq.gz \
+        -O {output_dir}/{key}_R2_trimmed.fastq.gz \
+        -j {output_dir}/{key}.fastp.json \
+        -h {output_dir}/{key}.fastp.html \
+        --unpaired1 {output_dir}/{key}_R1_unpaired.fastq.gz \
+        --unpaired2 {output_dir}/{key}_R2_unpaired.fastq.gz \
+        -l 25 \
+        -q 20")
+    fastp_runner.run_shell()
