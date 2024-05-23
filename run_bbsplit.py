fae068b1316b9ddfcec1a8c34ae81135b9da6649
Thomas Nipper <nipper@wisc.edu>
2024-05-23 11:03:13 -0500

    Refactor run_bbsplit.py to use FileHandler class for file handling and improve code readability

--- run_bbsplit.py
+++ run_bbsplit.py
@@ -7,25 +7,33 @@ ribodepleted_dir = f"{basedir}/bbduk_reads"
 out_dir = f"{basedir}/bbsplit_reads"
 ref_file = f"{basedir}/myco_genome.fasta"
 genome = f"{basedir}/genome/hybrid_genome.fasta"
-index_dir = f"{basedir}/bbsplit_reads/index"
-file_filter = lambda x: "nonrRNA" in x and x.endswith(".fastq.gz")
+index_dir = f"{basedir}/ref"
+file_filter = lambda x: "nonrRNA" in x
 CPU = os.cpu_count()
 
-files = FileHandler.get_files(ribodepleted_dir, "_R1", "_R2", file_filter = file_filter)
+ribodepleted_handler = FileHandler(ribodepleted_dir)
+files = ribodepleted_handler.get_files("_R1", "_R2", file_filter=file_filter)
 
 out_dir_handler = FileHandler(out_dir)
 out_dir_handler.make_dir()
 
+index_handler = FileHandler(index_dir)
+if not os.path.exists(index_dir):
+    index_handler.make_dir()
+    index_runner = ShellProcessRunner(f"bbsplit.sh ref_hybrid_genome={genome} ref_myco_genome={ref_file}")
+    index_runner.run_shell()
+
+
 for key in files:
-    command = f"bbsplit.sh \
+    command = ShellProcessRunner(
+                f"bbsplit.sh \
                 in={files[key][0]} \
                 in2={files[key][1]} \
                 outu1={out_dir}/{key}_R1_unmapped.fastq.gz \
                 outu2={out_dir}/{key}_R2_unmapped.fastq.gz \
-                ref_myco_genome={ref_file} \
-                ref_hybrid_genome={genome} \
                 refstats={out_dir}/{key}_refstats.txt \
                 threads={CPU} \
                 basename={out_dir}/{key}_#_%.fastq.gz \
-                -Xmx44g"
-    runner = ShellProcessRunner(command)
+                -Xmx60g"
+                )
+    command.run_shell()
