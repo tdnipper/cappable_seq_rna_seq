57b171a6bcd8e6467785d56ca6272101bf50d56d
Thomas Nipper <nipper@wisc.edu>
2024-05-24 09:05:59 -0500

    Refactor run_salmon_star.py to use ISR library type for salmon quant

--- run_salmon_star.py
+++ run_salmon_star.py
@@ -24,5 +24,5 @@ star_dir_handler.check_exists("Error: star alignments directory not found, run s
 files = FileHandler.get_files(star_dir_handler, "_Aligned.toTranscriptome.out.bam", file_filter=file_filter)
 
 for name in files:
-    salmon_runner = ShellProcessRunner(f"salmon quant -t {transcripts} -l A -a {files[name][0]} -p {CPU} -o {salmon_dir}/{name} --numBootstraps 30")
+    salmon_runner = ShellProcessRunner(f"salmon quant -t {transcripts} -l ISR -a {files[name][0]} -p {CPU} -o {salmon_dir}/{name} --numBootstraps 30")
     salmon_runner.run_shell()
