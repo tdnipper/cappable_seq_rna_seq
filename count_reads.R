8fdddf5de91a1b9584e747a45f25ad63468a29e1
Thomas Nipper <nipper@wisc.edu>
2024-05-27 10:32:27 -0500

    feat: Install required Bioconductor packages and process RNA-seq data

--- count_reads.R
+++ count_reads.R
@@ -3,6 +3,34 @@ if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
 BiocManager::install("DESeq2")
-library(DESeq2)
+BiocManager::install("tximport", dependencies = TRUE)
+BiocManager::install("tximeta", dependencies = TRUE)
 
-install.packages("tximport", dependencies = TRUE)
+# Start here once packages are installed
+
+library(DESeq2)
+library(tximeta)
+library(tools)
+
+# Needs samples.txt with sample names in $names column
+samples <- read.table("samples.txt", header = TRUE)
+samples <- data.frame(samples)
+
+# Needs salmon_quantification directory with salmon quant.sf files
+dir <- normalizePath("salmon_quantification")
+files <- file.path(dir, paste0(samples$names, "/", samples$names, "_quant.sf"))
+samples$files <- files
+View(samples)
+file.exists(samples$files)
+
+# Make custom linked txome from salmon index to get hybrid WSN/human
+txome <- makeLinkedTxome("genome/salmon_index",
+    "de-novo",
+    "Homo sapiens and WSN",
+    "custom", "GRCh38/WSN",
+    "genome/hybrid_exon.fasta",
+    "genome/hybrid_annotated_cat.gtf",
+    "tx.json"
+)
+
+se <- tximeta(samples)
