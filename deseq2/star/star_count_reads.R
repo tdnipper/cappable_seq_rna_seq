dd91dc81c8b2b84bf18fe46e71231a8e090c486c
Thomas Nipper <nipper@wisc.edu>
2024-05-31 13:45:58 -0500

    Refactor code to use consistent library import style and update file paths

--- deseq2/star/star_count_reads.R
+++ deseq2/star/star_count_reads.R
@@ -1,3 +1,15 @@
-library('DESeq2')
-library('tximeta')
-library('tools')
+library("DESeq2")
+library("tximeta")
+library("tools")
+
+# Needs samples.txt with sample names in $names column
+samples <- read.csv("samples.csv", header = TRUE)
+samples <- data.frame(samples)
+samples$condition <- factor(samples$condition)
+samples$infection <- factor(samples$infection)
+
+# read in star genecounts
+dir <- normalizePath("/home/ubuntu/blockvolume/cappable_seq_rna_seq/deseq2_star")
+infile <- file.path(dir, "deseq2_input.csv")
+
+dds_infection <- DESeqDataSetFromMatrix(countData = infile, colData = samples, ~ infection)
