921e6a0acc3e33939ae9e1a68d8e264b15061562
Thomas Nipper <nipper@wisc.edu>
2024-06-03 12:45:06 -0500

    read in star_data in alphabetical order

--- deseq2/star/star_count_reads.R
+++ deseq2/star/star_count_reads.R
@@ -5,11 +5,22 @@ library("tools")
 # Needs samples.txt with sample names in $names column
 samples <- read.csv("samples.csv", header = TRUE)
 samples <- data.frame(samples)
+samples$names <- as.character(samples$names)
+
+# sample$names in alphabetical order to match order of columns from infile
+samples <- samples[order(samples$names), ]
 samples$condition <- factor(samples$condition)
 samples$infection <- factor(samples$infection)
+View(samples)
 
-# read in star genecounts
-dir <- normalizePath("/home/ubuntu/blockvolume/cappable_seq_rna_seq/deseq2_star")
-infile <- file.path(dir, "deseq2_input.csv")
+# read in star genecounts as matrix (not dataframe)
+dir <- normalizePath("/home/ubuntu/blockvolume/cappable_seq_rna_seq/deseq2/star")
+infile_path <- file.path(dir, "deseq2_input.csv")
+infile <- as.matrix(read.csv(infile_path, row.names="gene_id"))
+
+# drop x column (index from old dataframe)
+infile <- infile[, -which(colnames(infile) %in% "X")]
+View(infile)
+
 
 dds_infection <- DESeqDataSetFromMatrix(countData = infile, colData = samples, ~ infection)
