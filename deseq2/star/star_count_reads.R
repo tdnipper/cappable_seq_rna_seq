1023b384065751eed4a22a19eae7dd1e1a114b08
Thomas Nipper <nipper@wisc.edu>
2024-06-04 14:22:59 -0500

    Remove qc commands and add updatead design param to account for enrichment from cappable seq when calculating infection log2FoldChange

--- deseq2/star/star_count_reads.R
+++ deseq2/star/star_count_reads.R
@@ -10,7 +10,6 @@ samples$names <- as.character(samples$names)
 samples <- samples[order(samples$names), ]
 samples$condition <- factor(samples$condition)
 samples$infection <- factor(samples$infection)
-View(samples)
 
 # read in star genecounts as matrix (not dataframe)
 dir <- normalizePath("/home/ubuntu/blockvolume/cappable_seq_rna_seq/deseq2/star")
@@ -19,30 +18,32 @@ infile <- as.matrix(read.csv(infile_path, row.names="gene_id"))
 
 # drop x column (index from old dataframe)
 infile <- infile[, -which(colnames(infile) %in% "X")]
-View(infile)
 
+# Set up DE experiment using infection status as the main condition
+# Use cappable-seq enrichment as additional factor
+dds_infection <- DESeqDataSetFromMatrix(countData = infile, colData = samples, ~ condition +  infection)
 
-dds_infection <- DESeqDataSetFromMatrix(countData = infile, colData = samples, ~ infection)
-dds_condition <- DESeqDataSetFromMatrix(countData = infile, colData = samples, ~ condition)
+# Specify the reference levels in each experimental factor
+dds_infection$infection <- relevel(dds_infection$infection, ref = "mock")
+dds_infection$condition <- relevel(dds_infection$condition, ref = "input")
 
-# Pre filter 
+# Pre filter genes that have < 10 reads across 2 samples
 smallest_group_size <- 2
 keep_infection <- rowSums(counts(dds_infection) >= smallest_group_size) >= 10
 dds_infection <- dds_infection[keep_infection, ]
 
-#Set up factors
-dds_infection$infection <- relevel(dds_infection$infection, ref = "mock")
-
 # Calculate differential expression
 dds_infection <- DESeq(dds_infection)
+resultsNames(dds_infection)
 res_infection <- results(dds_infection)
-summary(res_infection)
 
-# Filter by padj values to get significant results
+# Apply FDR filter of 0.05 to results 
 res05_infection <- results(dds_infection, alpha = 0.05)
-sum(res05_infection$padj < 0.05, na.rm = TRUE) # Returns 2425 genes
+sum(res05_infection$padj < 0.05, na.rm = TRUE) # Returns # of genes that pass FDR alpha
 summary(res05_infection)
 
-# Export for plotting
+# Export for plotting in csv
 res05_infection <- res05_infection[order(-res05_infection$log2FoldChange), ]
 write.csv(res05_infection, file = "deseq2/star/geneCounts_infection.csv", row.names = TRUE)
+significant <- subset(res05_infection, padj <= 0.05)
+write.csv(significant, file = "deseq2/star/geneCounts_infection_significant.csv", row.names = TRUE)
