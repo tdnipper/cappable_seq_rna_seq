34e74d96924182036eb2801a4985562050b234a5
Thomas Nipper <nipper@wisc.edu>
2024-06-04 14:21:35 -0500

    Graph both filtered and unfiltered data from genecounts

--- deseq2/salmon_only/graph_deseq2.py
+++ deseq2/salmon_only/graph_deseq2.py
@@ -3,8 +3,11 @@ import pandas as pd
 import seaborn as sns
 
 # Load the data
-data_condition = pd.read_csv("res05_condition_annotated.csv")
-data_infection = pd.read_csv("res05_infection_annotated.csv")
+data_condition = pd.read_csv("res05_condition.csv")
+filtered_condition = data_condition[data_condition["padj"] <= 0.05].dropna()
+data_infection = pd.read_csv("res05_infection.csv")
+filtered_infection = data_infection[data_infection["padj"] <= 0.05].dropna()
+
 
 # Plot the data
 sns.set_theme(style="whitegrid")
@@ -13,7 +16,7 @@ fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 # Plot for data_condition
 ax1 = axes[0]
 sns.scatterplot(
-    data=data_condition,
+    data=filtered_condition,
     x="log2FoldChange",
     y="baseMean",
     size="lfcSE",
@@ -26,7 +29,7 @@ ax1.set_title("Cappable-seq recovery")
 # Plot for data_infection
 ax2 = axes[1]
 sns.scatterplot(
-    data=data_infection,
+    data=filtered_infection,
     x="log2FoldChange",
     y="baseMean",
     size="lfcSE",
@@ -39,11 +42,16 @@ ax2.set_title("Infection")
 plt.tight_layout()
 plt.savefig("fc_vs_abundance.png", dpi=300)
 plt.close()
-# Plot data for both log2FC
-fig2 = sns.regplot(
-    x=data_condition["log2FoldChange"],
-    y=data_infection["log2FoldChange"],
-)
-fig2.set(xlabel="Cappable-seq log2FoldChange", ylabel="Infection log2FoldChange")
-fig2.set_title("Log2FoldChange comparison Capable-seq vs Infection")
-plt.savefig("log2fc_cappable_seq_vs_infection.png", dpi=300)
+
+
+# # Plot data for both log2FC
+# fig2 = sns.regplot(
+#     x=data_condition["log2FoldChange"],
+#     y=data_infection["log2FoldChange"],
+# )
+# fig2.set(xlabel="Cappable-seq log2FoldChange", ylabel="Infection log2FoldChange")
+# fig2.set_title("Log2FoldChange comparison Capable-seq vs Infection")
+# plt.savefig("log2fc_cappable_seq_vs_infection.png", dpi=300)
+
+# Plot -log10pvalues for infection data
+# filtered_infection['logp'] = 
