32ac2fd5e8b2f8407678947d85ab8130aaba6949
Thomas Nipper <nipper@wisc.edu>
2024-06-17 12:42:33 -0500

    assign group values based on WSN or human and color hue by groupvalue

--- deseq2/star/graph_genecounts_deseq2.py
+++ deseq2/star/graph_genecounts_deseq2.py
@@ -6,9 +6,25 @@ import numpy as np
 # Load the data
 data_infection = pd.read_csv("geneCounts_infection.csv")
 filtered_infection = data_infection[data_infection["padj"] <= 0.05].copy()
+# Get the current list of column names
+columns = filtered_infection.columns.tolist()
+
+# Replace the first column name with 'gene_id'
+columns[0] = 'gene_id'
+
+# Assign the modified list back to the DataFrame
+filtered_infection.columns = columns
 filtered_infection.loc[:, "p_adj_log"] = -np.log(filtered_infection["padj"])
+def get_group(gene_id, string):
+    if gene_id.startswith(f"{string}"):
+        return 1
+    else: 
+        return 2
+filtered_infection.loc[:, "group"] = filtered_infection.apply(lambda row: get_group(row['gene_id'], "WSN"), axis=1)
 filtered_infection.to_csv("filtered_geneCounts_significant.csv", index=False)
+
 # Plot the data
+colors = ["#aeccdb", "#3274a1"]
 sns.set_theme(style="white")
 fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
@@ -49,7 +65,9 @@ sns.scatterplot(
     x = "p_adj_log",
     y = "log2FoldChange",
     linewidth = 0,
-    legend = False
+    legend = False,
+    hue="group",
+    palette=colors
 )
 plt.xlabel("-log10 P value")
 plt.ylabel("Log2 foldchange")
